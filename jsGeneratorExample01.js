3bee5bae691bf2cdac58894b3213f8bd0b0a274d
rbtadikonda <45958192+rbtadikonda@users.noreply.github.com>
2022-02-05 16:14:13 -0600

    Update jsGeneratorExample01.js

--- jsGeneratorExample01.js
+++ jsGeneratorExample01.js
@@ -1,22 +1,23 @@
 //EXAMPLE 1:
 let ask1 ,
     ask2
+
 function* gen() {
   ask1 = yield "2 + 2 = ?";
 
-  console.log(`ask1 = ${ask1}, ask1 gets its value from LINE 3 with next(4)`); // 4
+  console.log(`ask1 = ${ask1}, ask1 gets its value from LINE 3 with next(4)`); //prints  4
 
   ask2 = yield "3 * 3 = ?"
 
-  console.log(`ask2 = ${ask2}, ask2 gets its value from LINE 5 with next(9)`); // 9
+  console.log(`ask2 = ${ask2}, ask2 gets its value from LINE 5 with next(9)`); // prints 9
 }
 
 let generator = gen();
 
-console.log( `LINE 1 => ${generator.next().value}` ); // "2 + 2 = ?"
+console.log( `LINE 1 => ${generator.next().value}` ); // prints "2 + 2 = ?"
 console.log( `       ask1 = ${ask1}, ask1 will be undefined here`)
 
-console.log( `LINE 3 => ${generator.next(4).value}` ); // "3 * 3 = ?"
+console.log( `LINE 3 => ${generator.next(4).value}` ); // prints "3 * 3 = ?"
 console.log( `       ask2 = ${ask2}, ask2 will be undefined here`)
 
-console.log( `LINE 5 => ${generator.next(9).done}` ); // true
+console.log( `LINE 5 => ${generator.next(9).done}` ); // prints true
